50e395126b6e7f4b94cb1ea4cc08532e8db52822
AdamDanieli <35630081+AdamDanieli@users.noreply.github.com>
2018-01-20 19:40:21 +0100

    Update and rename View.java to Chatview.java

---
+++ Chatview.java
@@ -0,0 +1,10 @@
+
+
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+public Class Chatview 
