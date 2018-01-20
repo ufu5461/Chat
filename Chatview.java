25e9c9cf9134fcd5276161889317b5f524c5a501
AdamDanieli <35630081+AdamDanieli@users.noreply.github.com>
2018-01-20 20:17:52 +0100

    Update Chatview.java

--- Chatview.java
+++ Chatview.java
@@ -1,5 +1,3 @@
-
-
 import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
@@ -7,14 +5,19 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
-public Class Chatview{
+public Class Chatview implements ActionListener{
 private JPanel writingPanel = New JPanel
 private JFrame chatWindow = New JFrame
 private JScrollPanel messagePanel = New JScrollPanel
-private JButton sendButton = New JButton
+private JButton sendButton = New JButton("Send")
   
   public Chatview(Controller c){
     
+    
   }
+  sendButton.addActionListener(this)
+  
+  public void actionPerformed(ActionEvent e){
+    c.updatechatViewer }
 }
 
