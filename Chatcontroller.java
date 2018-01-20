7f083dc2c86b8fbd7e8014ee9307205e8d8b7f0c
AdamDanieli <35630081+AdamDanieli@users.noreply.github.com>
2018-01-20 20:35:55 +0100

    Update Chatcontroller.java

--- Chatcontroller.java
+++ Chatcontroller.java
@@ -1,6 +1,6 @@
 \\ importera paket
 
-public Class Chatcontroller implements ActionListerner{
+public Class Chatcontroller{
 
 public Chatcontroller(Chatt c){
   public Chatviewer = New Chatviewer(this)
