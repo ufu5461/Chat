01c9aae715eabca665c35c159d3f0536eeb550c8
ufu5461 <35562909+ufu5461@users.noreply.github.com>
2018-01-20 18:46:34 +0100

    Update Chatt.java

--- Chatt.java
+++ Chatt.java
@@ -1,33 +1,53 @@
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Chatt {
+public class Chatt implements Runnable {
+	private String chattName;
 	private List<User> users; 
-	private List<String> messages;
+	private List<Message> messages;
 	
-	public Chatt(String user, BufferedReader in, PrintWriter out) {
+	public Chatt(String name) {
 		this.users = new ArrayList<User>();
-		this.messages = new ArrayList<String>();
-		addUser(user, in, out);
+		this.messages = new ArrayList<Message>();
+		chattName = name;
 	}
 	
-	public void addUser(String user, BufferedReader in, PrintWriter out) {
-		User tempuser = new User(user, in, out);
-		users.add(tempuser);
+	public void addUser(User us) {
+		users.add(us);
 	}
 	
-	public void removeUser(User user) {
-		users.remove(users.indexOf(user));
+	public void removeUser(User us) {
+		users.remove(us);
 	}
 	
-	public void addMessage(String message) {
-		messages.add(message);
+	public void sendMessage(String msg) {
+		for(int i = 0; i < users.size(); i++) {
+			users.get(i).out.println(msg);
+		}
 	}
 	
-	public List<String> getMessages(){
-		return messages;
+	public Message recieveMessage(String msg) {
+		return new Message(msg);
+	}
+
+	@Override
+	public void run() {
+		String input;
+		while(true) {
+			for(int i = 0; i < users.size(); i++) {
+				try {
+				if((input = users.get(i).in.readLine()) != null) {
+					recieveMessage(input);
+				}
+				}catch(Exception e) {
+					
+				}
+			}
+		}
 	}
 	
+		
 }
