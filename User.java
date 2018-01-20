99082dcf5c426ddee63da61acebf821614f08175
ufu5461 <35562909+ufu5461@users.noreply.github.com>
2018-01-20 17:55:34 +0100

    Update and rename Users.java to User.java

--- User.java
+++ User.java
@@ -1,14 +1,17 @@
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.net.Socket;
 
 public class User {
 	String name;
 	BufferedReader in;
 	PrintWriter out;
+	Socket s;
 	
-	public User(String name, BufferedReader in, PrintWriter out) {
+	public User(String name, BufferedReader in, PrintWriter out, Socket s) {
 		this.name = name;
 		this.in = in;
 		this.out = out;
+		this.s = s;
 	}
 }
