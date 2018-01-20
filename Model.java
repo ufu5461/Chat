fe78f08c51c05f16ec1843496fbb689f9f3166d9
ufu5461 <35562909+ufu5461@users.noreply.github.com>
2018-01-20 15:54:06 +0100

    Update Model.java

--- Model.java
+++ Model.java
@@ -1,5 +1,37 @@
-public class Model{
-  public Model(){
-  
-  }
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+
+// Connection to other clients here, message send and receive
+public class Model {
+	
+	// List of chats
+	
+	// Create new chats if client
+	public boolean connectTo(String hostName, int portNumber) {
+		try {
+			    Socket echoSocket = new Socket(hostName, portNumber);
+			    PrintWriter out =
+			        new PrintWriter(echoSocket.getOutputStream(), true);
+			    BufferedReader in =
+			        new BufferedReader(
+			            new InputStreamReader(echoSocket.getInputStream()));
+			    //BufferedReader stdIn =
+			     //   new BufferedReader(
+			      //      new InputStreamReader(System.in));
+		} catch(Exception e){
+			return false;
+		}
+		return true;
+
+	}
+	// Create new chats if server
+	
+	// Send message
+	
+	// Receive message
+	
+	
 }
