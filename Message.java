23dd74b6e8ecfb43daafa343c2a8de4ed2d7b5a5
AdamDanieli <35630081+AdamDanieli@users.noreply.github.com>
2018-01-20 15:49:24 +0100

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,22 @@
+private class Message {
+  private String messengeSender
+  private int hourReceived
+  private int minuteReceived
+  private int rgbColour
+  private string message
+  
+  private Message (string messageSender,int hourReceived, int minuteReceived, int rgbColour, string message)  \\ 
+  
+  getSender(){
+  return messageSender}
+  
+  getTime(){
+  return hourReceiver + "." + minutereceived}
+  
+  getText(){
+  return message}
+  
+  getColour(){
+  return rgeColour}
+
+}
