085606ec3295f6cfa3d0364e152d25e8a24228da
AdamDanieli <35630081+AdamDanieli@users.noreply.github.com>
2018-01-20 20:06:40 +0100

    Update Chatview.java

--- Chatview.java
+++ Chatview.java
@@ -7,4 +7,14 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
-public Class Chatview 
+public Class Chatview{
+private JPanel writingPanel = New JPanel
+private JFrame chatWindow = New JFrame
+private JScrollPanel messagePanel = New JScrollPanel
+private JButton sendButton = New JButton
+  
+  public Chatview(Controller c){
+    
+  }
+}
+
