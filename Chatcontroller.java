537e0919e289cc2ffb5b333481cecd7865f53739
AdamDanieli <35630081+AdamDanieli@users.noreply.github.com>
2018-01-23 16:42:22 +0100

    Update Chatcontroller.java

--- Chatcontroller.java
+++ Chatcontroller.java
@@ -5,34 +5,28 @@ import java.awt.event.ActionListener;
 public Class Chatcontroller implements Actionlistern  er{
 private Chatviewer v;
 private Chat ch;
-
   
 public Chatcontroller(Chat ch){
   this.v = New Chatviewer(this)
   this.ch=ch;
   
- public void updatechatViewer(){ \\printa ut ny meddelandehistorik
-  List<Message> list = ch.getMessages();
+public void updateView(){
+  List<Message> previousMessages = ch.getMessages
+}
             
-                                
-                               }
 @Override
  public void actionPerformed(ActionEvent e){
    String command = e.getActionCommand();
-   if(command =="Send"){
+   if(command == "Send"){
    
-   ch.addmessage(v. det som står i meddelanderutan); 
-    v. cleara meddelanderutan;
-    updatechatviewer();
-   }
+    ch.addmessage(v.exportMessage, v.exportColour, String "Me")
+    this.updateView();
+    v.clearSendwindow();
    else{
-   this.closeDown();
-   ch.closeChat();
+   v.exitView;
    }
    
-     
-     
- public void closeDown(){
+  public void closeDown(){
   v.setVisible(false);
   v.dispose();
   ch.closeChat(); 
