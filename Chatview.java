c4cd4f60fca427b168a43437ae71296d93f29cc6
AdamDanieli <35630081+AdamDanieli@users.noreply.github.com>
2018-01-23 13:56:07 +0100

    Update Chatview.java

--- Chatview.java
+++ Chatview.java
@@ -1,3 +1,19 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
@@ -5,19 +21,34 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
-public Class Chatview extends JFrame implements ActionListerner{
-private JPanel writingPanel = New JPanel
-private JScrollPanel messagePanel = New JScrollPanel;
+public Class Chatview extends JFrame{
+private Controller c;
+private JTextField messageBox = New JTextField;
+private JTextArea messagePanel = New JTextArea;
 private JButton sendButton = New JButton("Send");
+private JButton leaveButton = New JButton("Leave chat");
   
-  public Chatview(Controller c, String name){
+  public Chatview(Controller con, String name){
     super(name);
-    
+    this.c = con;
+    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    this.setSize(500, 500);
+    this.add(messagepanel);
+    this.add(sendbutton);
+    this.add(writingPanel);
+    setVisible(true);
+    sendButton.addActionListener(c)   
     
   }
-  sendButton.addActionListener(this) \\eller ska det vara till controllern?
   
-  public void actionPerformed(ActionEvent e){
-    c.updatechatViewer }
+public void updateChatview(List<Message> mlist){}
+  
+public void clearSendwindow(){}
+  
+public void leaveChat(){}
+  
+public void exportMessage(){}
+  
+
 }
 
