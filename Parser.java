674ea67caf369a38f418ffe44f71e9e5a01ca446
ufu5461 <35562909+ufu5461@users.noreply.github.com>
2018-01-23 17:36:57 +0100

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,1 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Parser{
+	
+	public Parser() {
+		
+	}
+	
+	public String parseXML(String XML) {
+		
+		// Remove unsupported tags
+		XML = XML.replaceAll("<fetstil>", "").
+				replaceAll("</fetstil>","").
+				replaceAll("<kursiv>","").
+				replaceAll("</kursiv>","");
+		
+		// Check for disconnect tag <disconnect />
+		
+		// Check for color attribute <text color="#RRGGBB">MessageText</text>
+		
+		// Check for encryption <encrypted type="AES" key="publicKey">EncryptedText</encrypted>
+		
+		// File transfer <filerequest name="filename" size="filesize">Textaboutfile</filerequest>
+		// Wait for file response <fileresponse answer="yes">
+		
+		// Check for tag that request connection.
+		return null;
+	}
+}
