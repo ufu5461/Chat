0b0bbc6b31be741fce82f1955bd58119f79eeb97
ufu5461 <35562909+ufu5461@users.noreply.github.com>
2018-01-23 14:34:31 +0100

    Create ClientController.java

---
+++ ClientController.java
@@ -0,0 +1,35 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientController implements ActionListener {
+	private ClientView v;
+	private ClientModel m;
+	
+	public ClientController() {
+		v = new ClientView(this);
+		m = new ClientModel();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String command = e.getActionCommand();
+		String host = v.getHost();
+		int port = v.getPort();
+		if(port == -1) {
+			return;
+		}
+		
+		if("Public Chat".equals(command)) {
+			if(!m.connectTo(host, port, true)) {
+				v.connectionError();
+			};
+		}
+		else if("Private Chat".equals(command)) {
+			if(!m.connectTo(host, port, false)) {
+				v.connectionError();
+			}
+		}
+		
+	}
+}
