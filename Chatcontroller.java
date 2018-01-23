3864885ce6966c106e4e741545dbfd740164c71f
AdamDanieli <35630081+AdamDanieli@users.noreply.github.com>
2018-01-23 17:14:37 +0100

    Update Chatcontroller.java

--- Chatcontroller.java
+++ Chatcontroller.java
@@ -10,30 +10,25 @@ public Chatcontroller(Chat ch){
   this.v = New Chatviewer(this)
   this.ch=ch;
   
+               \\     public void updateView(){
+               \\     List<Message> previousMessages = ch.getMessages
+               \\     StringBuilder stringBuilder = new StringBuilder();
+               \\     for(int i=0, i < previousMessages.size(), i++){
+               \\     stringBuilder.append(previousMessages[i].getHtml())
+               \\     };
+               \\     String finalHtml = stringBuilder.toString();
+               \\     v.publishChatView(finalHtml);} 
+  
 public void updateView(){
-  List<Message> previousMessages = ch.getMessages
-  StringBuilder stringBuilder = new StringBuilder();
-  for(int i=0, i < previousMessages.size(), i++){
-      stringBuilder.append(previousMessages[i].getHtml())
-  }
-String finalHtml = stringBuilder.toString();
-v.publishChatView(finalHtml)
+  List<Message> previousMessages = ch.getMessages();
+    Message lastMessage = previousMessages.get(previousMessages.size() - 1); 
+    v.publishChatview(lastmessage.getHtml());
 }
-            
-  
-  
-  
-  
-  
-  
-  
-  
   
 @Override
  public void actionPerformed(ActionEvent e){
    String command = e.getActionCommand();
    if(command == "Send"){
-   
     ch.addmessage(v.exportMessage, v.exportColour, String "Me")
     this.updateView();
     v.clearSendwindow();
