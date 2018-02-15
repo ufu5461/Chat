4a8608dbd1624089ed158a0fed88056c9db8f312
ufu5461 <35562909+ufu5461@users.noreply.github.com>
2018-02-15 10:55:34 +0100

    Update Chatcontroller.java

--- Chatcontroller.java
+++ Chatcontroller.java
@@ -1,53 +1,53 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
-public Class Chatcontroller implements Actionlisterner{
-private Chatviewer v;
-private Chat ch;
-  
-public Chatcontroller(Chat ch){
-  this.v = New Chatviewer(this)
-  this.ch=ch;
-  
-               \\     public void updateView(){
-               \\     List<Message> previousMessages = ch.getMessages
-               \\     StringBuilder stringBuilder = new StringBuilder();
-               \\     for(int i=0, i < previousMessages.size(), i++){
-               \\     stringBuilder.append(previousMessages[i].getHtml())
-               \\     };
-               \\     String finalHtml = stringBuilder.toString();
-               \\     v.publishChatView(finalHtml);} 
-  
-public void updateView(){
-  List<Message> previousMessages = ch.getMessages();
-    Message lastMessage = previousMessages.get(previousMessages.size() - 1); 
-    v.publishChatview(lastmessage.getHtml());
-}
-  
-@Override
- public void actionPerformed(ActionEvent e){
-   String command = e.getActionCommand();
-   if(command == "Send"){
-     try 
-    ch.addmessage(v.exportMessage, v.exportColour, String "Me")
-    this.updateView();
-    v.clearSendwindow();
-   else{
-   v.exitView;
-   }
-   
-  public void closeDown(){
-  v.setVisible(false);
-  v.dispose();
-  ch.closeChat(); 
-    }
-   
-   
+public class Chatcontroller implements ActionListener{
+	private Chatviewer v;
+	private Chat ch;
+	  
+	public Chatcontroller(Chat ch){
+	  this.v = new Chatviewer(this);
+	  this.ch=ch;
+	  
+	               //     public void updateView(){
+	               //     List<Message> previousMessages = ch.getMessages
+	               //     StringBuilder stringBuilder = new StringBuilder();
+	               //     for(int i=0, i < previousMessages.size(), i++){
+	               //     stringBuilder.append(previousMessages[i].getHtml())
+	               //     };
+	               //     String finalHtml = stringBuilder.toString();
+	               //     v.publishChatView(finalHtml);} 
+	}
+	  
+	public void updateView(){
+	  List<Message> previousMessages = ch.getMessages();
+	    Message lastMessage = previousMessages.get(previousMessages.size() - 1); 
+	    v.publishChatview(lastMessage.getHTML());
+	}
+	  
+	@Override
+	 public void actionPerformed(ActionEvent e){
+	   String command = e.getActionCommand();
+	   if(command == "Send") {
+		   try {
+			   ch.addMessage(v.exportMessage, v.exportColour, "Me");
+			   this.updateView();
+			   v.clearSendWindow();
+		   }catch(Exception e) {
+			   e.printStackTrace();
+		   }
+	   }
+	   else {
+		   v.exitView();
+	   }
+	}
+	   
+	  public void closeDown(){
+		  v.setVisible(false);
+		  v.dispose();
+		  ch.closeChat(); 
+	    }
  
- 
-}
-}
-
-
 }
