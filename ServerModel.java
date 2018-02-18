312fa647937474688ff47844486b92f3a84078b7
ufu5461 <35562909+ufu5461@users.noreply.github.com>
2018-02-18 23:44:22 +0100

    Update ServerModel.java

--- ServerModel.java
+++ ServerModel.java
@@ -7,25 +7,25 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 // Connection to other clients here, message send and receive
 public class ServerModel {
 	
 	int portNumber;
-	String myName;
+	String myName = "Server";
 	ServerSocket serverSocket;
 	private static int maxConnections = 10;
 	private static User[] users = new User[maxConnections];
 	private static Thread[] chatsThread = new Thread[maxConnections];
 	private static Chat[] chats = new Chat[maxConnections];
+	private ConnectionManager manager;
+	private PopUps pop = new PopUps();
 	
 	public ServerModel() {
 		portNumber = 8080;
-		// Open new server connection
-		try{
-		      serverSocket = new ServerSocket(portNumber);
-		    } catch (IOException e) {
-		    	  e.printStackTrace();
-		    }
+		manager = new ConnectionManager(myName);
+		manager.setServerSocket(portNumber);
 		// Create a public chat in Thread[0]
 		chats[0] = new Chat("Public: " + myName);
 		chatsThread[0] = new Thread(chats[0]);
@@ -34,77 +34,59 @@ public class ServerModel {
 		incomming();
 	}
 	
-	private String readName(BufferedReader in) {
+	private User allowConnection(BufferedReader in, PrintWriter out, Socket clientSocket) {
 		try {
-			while(true) {
-				String input = in.readLine();
-				if(input != null) {
-					System.out.println("Returned: " + input);
-					return input;
-				}
-		}
-		}catch(Exception e) {
-			e.printStackTrace();
-			return "Error";
-		}
-	}
-	
-	private boolean wantPub(BufferedReader in) {
-		try {
-		while(true) {
 			String input = in.readLine();
-			System.out.println("Input: " + input);
-			if(input != null) {
-				if(input.equals("True")) {
-					return true;
-				}
-				else {
-					return false;
-				}
+			System.out.println(input);
+			Parser p = new Parser();
+			Message msg = new Message();
+			msg = p.parseXML(input,msg);
+			boolean response;
+			String name;
+			User usr;
+			if(msg.isUnknown()) {
+				String content = "Connection request from an unknown user";
+				name = "Unknown";
+				response = pop.connectionRequest(content, name);
+				usr = new User(name,in,out,clientSocket);
+				usr.setPub(true);
+			}else {
+				System.out.println("Message was known");
+				name = msg.getSender();
+				response = pop.connectionRequest(msg.getContent(), name);
+				usr = new User(name, in, out, clientSocket);
+				usr.setPub(msg.getPub());
 			}
-		}
+			if(response) {
+				return usr;
+			}else {
+				usr.sendRefused();
+				usr.closeConnection();
+				manager.closeConnection(in, out, clientSocket);
+				return null;
+			}
 		}catch(Exception e) {
 			e.printStackTrace();
-			return false;
 		}
+		return null;
 	}
 	
 	public void incomming()  {
-			int i = 0;
-		  	while(true) {
-		  		try {
-		  			BufferedReader in;
-		  			PrintWriter out;
-		  			// Wait for new connection
-		  			System.out.println("Waiting for connection");
-					Socket clientSocket = serverSocket.accept();
-		  			System.out.println("Incomming connection");
-					
-					// If more connections available create a user
-					if(i < maxConnections) {
-						System.out.println("Trying to set up connection");
-						out = new PrintWriter(clientSocket.getOutputStream(), true);
-						in = new BufferedReader(
-							        new InputStreamReader(clientSocket.getInputStream()));
-						System.out.println("Initialized out/in streams");
-						// Assumes first input name next public bool
-						String name = readName(in);
-						System.out.println(name);
-						users[i] = new User(name, in, out, clientSocket);
-						if(wantPub(in)) {
-							chats[0].addUser(users[i]); // Add to public chat
-						}else { // Create private chat
-							chats[i] = new Chat("Private: " + myName + " and " + name);
-							chatsThread[i] = new Thread(chats[i]);
-							chatsThread[i].start();
-						}
-					}
-					
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+		int i = 0;
+		  while(true) {
+		  	User usr = manager.incomming();
+		  	usr = allowConnection(usr.getIn(),usr.getOut(),usr.getS());
+		  	if(usr != null) {
+				users[i] = usr;
+				if(usr.getPub()) {
+					chats[0].addUser(users[i]); // Add to public chat
+				}else { // Create private chat
+					chats[i] = new Chat("Private: " + myName + " and " + usr.getName());
+					chats[i].addUser(usr);
+					chatsThread[i] = new Thread(chats[i]);
+					chatsThread[i].start();
 				}
-		  	}
-		
-	}
+			}		
+	  	}
+	}	
 }
