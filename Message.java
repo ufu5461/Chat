6d322a35813bd2fcf57f706af9380141ae2c3e7b
ufu5461 <35562909+ufu5461@users.noreply.github.com>
2018-02-18 23:43:24 +0100

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,28 +1,86 @@
-private class Message {
-  private String messengeSender
-  private int hourReceived
-  private int minuteReceived
-  private int rgbColour
-  private String message
-  
-  private Message (User user,int hour, int minuteReceived, int colour, string parsedMessage){
-    this.messegeSender = user.getName();
-    this.hourreceived = hour;
-    this.rgbColour = colour;
-    this.message = parsedMessage;
-  }  
-  
-  public String getSender(){
-  return messageSender;}
-  
-  public String getTime(){
-  return (hourReceived.toString() + "." + minutereceived.toString();
-          }
-  
-  public String getText(){
-  return message;}
-  
-  public int getColour(){
-  return rgeColour;}
+/*
+ * <message sender="Name">
+ * <text>
+ * This is the text of this message
+ * </text>
+ * </message>
+ * 
+ * 
+ */
+
+public class Message {
+	protected String content;
+	protected String XML;
+	protected String sender;
+	protected String rgb = "PLACEHOLDER";
+	protected String encrypted = "no";
+	protected boolean unknown = false;
+	protected boolean pub = false;
+	protected User usr;
+	
+	public Message() {
+		
+	}
+	
+	private String toHTML() {
+		String HTML = "<b>" + sender + ":<\b> <p>"+content+"</p>";
+		return HTML;
+	}
+	// Getters
+	public String getHTML() {
+		return sender + ": " + content;
+	}
+	public String getXML() {
+		return XML;
+	}
+	public String getContent() {
+		return content;
+	}
+	public String getEncryption() {
+		return encrypted;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public String getRgb() {
+		return rgb;
+	}
+	public boolean isUnknown() {
+		return unknown;
+	}
+	public boolean getPub() {
+		return pub;
+	}
+	public User sentBy() {
+		return usr;
+	}
+	
+	// Setters
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public void setXML(String XML) {
+		this.XML = XML;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public void setRgb(String rgb) {
+		this.rgb = rgb;
+	}
+	public void setEncryption(String encrypted) {
+		this.encrypted = encrypted;
+	}
+	public void setUnknown(boolean unknown) {
+		this.unknown = unknown;
+	}
+	public void setPub(String publ) {
+		if(publ.equals("True")) {
+			pub = true;
+		}
+	}
+	public void setSenderUser(User usr) {
+		this.usr = usr;
+	}
 
 }
