140a5a20131505c062c6b5920533500a1dde8abf
ufu5461 <35562909+ufu5461@users.noreply.github.com>
2018-02-18 23:44:48 +0100

    Update UserInputReader.java

--- UserInputReader.java
+++ UserInputReader.java
@@ -5,10 +5,12 @@ public class UserInputReader implements Runnable{
 	private User u;
 	private int userIndex;
 	private BufferedReader in;
+	private Parser p;
 
 	public UserInputReader(Chat c, User u, int i) {
 		this.c = c;
 		this.u = u;
+		p = new Parser();
 		userIndex = i;
 		in = u.getIn();
 		
@@ -23,7 +25,12 @@ public class UserInputReader implements Runnable{
 			try {
 				String input = in.readLine();
 				if(input != null) {
-					c.recieveMessage(input, userIndex);
+					Message msg = new Message();
+					System.out.print(input);
+					msg.setXML(input);
+					msg.setSenderUser(u);
+					msg = p.parseXML(input, msg);
+					c.recieveMessage(msg, userIndex);
 				}
 			}catch(Exception e) {
 				e.printStackTrace();
